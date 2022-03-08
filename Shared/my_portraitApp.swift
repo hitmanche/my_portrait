31276bdf2f796d612c82d027c941f8efd0bfbd81
Tayfun Akgün <tayfun_akgun@sestek.com>
2022-03-08 22:10:09 +0300

    Initial Commit

---
+++ Shared/my_portraitApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_portraitApp.swift
+//  Shared
+//
+//  Created by Tayfun on 8.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct my_portraitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
